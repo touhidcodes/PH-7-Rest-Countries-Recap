43ff7902280ab84e44d9bff89d0b1cc9386a397c
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-23 15:10:14 +0600

    recap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+const url = `https://restcountries.com/v3.1/all`;
 const loadData = async () => {
-	const res = await fetch("url");
+	const res = await fetch(url);
 	const data = await res.json();
-    console.log(data)
+	displayCountries(data);
+	console.log(data);
 };
 
+loadData();
+
+const displayCountries = (countries) => {
+	const countriesHTML = countries.map((country) => displayCountry(country));
+	const countriesID = document.getElementById("countries-container");
+	countriesID.innerHTML = countriesHTML.join(" ");
+};
+
+const displayCountry = (country) => {
+	return `
+	<div class="country">
+	<h2>${country.name.common}</h2>
+	<img src="${country.flags.png}">
+	</div>
+	`;
+};
